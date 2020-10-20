d977b0052011e7e2c5941cb132399acc9fe41af2
箕伯 <jibo.xf@alibaba-inc.com>
2020-10-20 13:12:22 +0800

    Add DLA Ganos

---
+++ ganos/README.md
@@ -0,0 +1,31 @@
+## DLA Ganos 测试案例
+
+### 数据准备
+Ganos案例部分数据已经包含在resource文件中
+
+SpatialJoin案例需要单独下载如下数据：
+* [LC08_L1TP_121035_20190702_20190706_01_T1.tiff](oss://dla-ganos-bj/public/LC08_L1TP_121035_20190702_20190706_01_T1.TIF)
+* [srtm_60_05.tiff](oss://dla-ganos-bj/public/srtm_60_05.tif)
+
+下载完成后将文件放在main/resources目录下。
+
+### 案例介绍
+
+DLA Ganos案例包含两部分：
+#### 数据源 DataSource
+包含DLA Ganos支持的数据源：
+* Lindorm
+* PolarDB
+* OSS
+* GeoTiff
+* GeoMesa
+
+#### 测试案例
+* LocalAlgorithm: 基本栅格代数运算 
+* NDVI: 自定义UDF，计算NDVI
+* Masking: 栅格数据掩膜(Masking)操作
+* OSS2HBase: 加载OSS栅格数据，进行拼接、冲投、创建金字塔等操作，最后写入HBase中
+* SpatialJoin: 将空间参考和范围不同的栅格数据按照空间进行Join操作
+* Classificaiton: 基于SparkML进行监督分类
+
+
---
+++ ganos/src/main/scala/com/aliyun/spark/ganos/OSS2HBase.scala
@@ -0,0 +1,71 @@
+package com.aliyun.spark.ganos
+
+import com.aliyun.ganos.dla._
+import com.aliyun.ganos.dla.geometry._
+import com.aliyun.ganos.dla.oss._
+import com.aliyun.ganos.dla.raster._
+import geotrellis.layer._
+import geotrellis.proj4.WebMercator
+import geotrellis.raster._
+import geotrellis.raster.resample.Bilinear
+import geotrellis.spark.TileLayerRDD
+import geotrellis.spark.pyramid.Pyramid
+import geotrellis.spark.store.hbase.HBaseLayerWriter
+import geotrellis.store._
+import geotrellis.store.hbase.{HBaseAttributeStore, HBaseInstance}
+import geotrellis.store.index.ZCurveKeyIndexMethod
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+
+/**
+  * 加载OSS中的GeoTiff文件为RDD，并写入Lindorm(HBase)
+  */
+object OSS2HBase extends App {
+
+  Logger.getLogger("org").setLevel(Level.ERROR)
+  Logger.getLogger("com").setLevel(Level.ERROR)
+  implicit val spark: SparkSession = {
+    val session = SparkSession.builder
+      .master("local[*]")
+      .withKryoSerialization
+      .config(additionalConf)
+      .getOrCreate()
+    session
+  }
+
+  import spark.implicits._
+
+  spark.withGanosGeometry
+  spark.withGanosRaster
+
+  //加载OSS文件
+  val uri1 = new java.net.URI("OSS地址/srtm_60_05.tif")
+  val uri2 = new java.net.URI("OSS地址/srtm_61_05.tif")
+  val options = Map(
+    "crs"->"EPSG:4326",
+    "endpoint" -> "OSS地址",
+    "accessKeyId" -> "客户accessKeyId",
+    "accessKeySecret" -> "客户accessKeySecret")
+  val rf1 = spark.read.ganos.oss(options).loadLayer(uri1)
+  val rf2 = spark.read.ganos.oss(options).loadLayer(uri2)
+  val df = rf1 union rf2
+  val rdd:(Int,TileLayerRDD[SpatialKey]) = df.asGanosRasterLayer.toTileLayoutRDD(st_geometry($"extent"), $"tile")
+  val tiles:TileLayerRDD[SpatialKey]= rdd._2
+
+  //定义Lindorm(HBase)数据源
+  val layerName="oss"
+  val instance = HBaseInstance(Seq("Lindorm连接地址"), "localhost")
+  val attributeStore = HBaseAttributeStore(instance, "SRTM")
+  val writer = HBaseLayerWriter(attributeStore, "SRTM_TILE")
+
+  val layoutScheme=ZoomedLayoutScheme(WebMercator, tileSize = 256)
+
+  //创建金字塔并写入Lindorm
+  Pyramid.upLevels(tiles, layoutScheme, rdd._1, Bilinear) { (rdd, z) =>
+    val layerId = LayerId(layerName, z)
+    writer.write(layerId, rdd, ZCurveKeyIndexMethod)
+  }
+
+  def additionalConf = new SparkConf(false)
+}
